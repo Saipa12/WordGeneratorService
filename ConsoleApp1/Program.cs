d99d7536069a9b449fafa59dcbd4d8db1d77fa0d
Saipa <Saipa@DESKTOP-1VNDJCG>
2023-12-26 21:49:43 +0300

    готово

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,12 +6,9 @@
 
 		try
 		{
-			// Шаг 1: Вызов микросервиса для получения случайного слова
 			string randomWord = await CallRandomWordService("http://localhost:6000/api/word");
 			Console.WriteLine($"Random Word: {randomWord}");
 
-			// Шаг 2: Другие действия или вызовы микросервисов
-			//await PerformOtherActions(randomWord);
 			randomWord = await CallRandomWordService("http://localhost:6001/api/word");
 			Console.WriteLine($"Random Word: {randomWord}");
 
@@ -19,7 +16,6 @@
 		}
 		catch (Exception ex)
 		{
-			// Шаг 3: Обработка ошибок и запуск компенсирующих действий
 			Console.WriteLine($"Saga failed: {ex.Message}");
 			await RollbackActions();
 		}
@@ -27,15 +23,6 @@
 
 	private static async Task<string> CallRandomWordService(string serviceUrl)
 	{
-		// Ваш код для вызова микросервиса RandomWordService
-		// Используйте HttpClient, gRPC или другие средства взаимодействия с микросервисом.
-		// Пример использования HttpClient:
-		// var httpClient = new HttpClient();
-		// var response = await httpClient.GetAsync("http://localhost:5000/api/RandomWord");
-		// return await response.Content.ReadAsStringAsync();
-		//return "SampleWord";
-		//string serviceUrl = "http://localhost:6000/api/word";
-
 		using HttpClient client = new();
 		try
 		{
@@ -58,16 +45,8 @@
 		}
 	}
 
-	//private static async Task PerformOtherActions(string randomWord)
-	//{
-	//	// Ваш код для выполнения других действий или вызова других микросервисов
-	//	// Можно сгенерировать ошибку для демонстрации сбоя
-	//	throw new InvalidOperationException("Simulated failure in other actions");
-	//}
-
 	private static async Task RollbackActions()
 	{
-		\
 		Console.WriteLine("Performing rollback actions...");
 	}
 }
